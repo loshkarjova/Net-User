57161e185d9e167d2bd3132fd9da6f9be56ee52e
loshkarjova <loshkarjova@gmail.com>
2023-06-15 12:55:45 +0300

    committed from local storage

---
+++ src/main/java/org/example/user/NettyUser.java
@@ -0,0 +1,52 @@
+package org.example.user;
+
+import io.netty.bootstrap.Bootstrap;
+import io.netty.channel.Channel;
+import io.netty.channel.ChannelFuture;
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.ChannelPipeline;
+import io.netty.channel.EventLoopGroup;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.channel.socket.nio.NioSocketChannel;
+import io.netty.handler.codec.string.StringDecoder;
+import io.netty.handler.codec.string.StringEncoder;
+
+public final class NettyUser {
+
+    static final String HOST = "127.0.0.1";
+    static final int PORT = 8001;
+
+    public static void main(String[] args) throws Exception {
+
+        EventLoopGroup group = new NioEventLoopGroup();
+
+        try {
+            Bootstrap b = new Bootstrap();
+            b.group(group)
+                    .channel(NioSocketChannel.class)
+                    .handler(new ChannelInitializer<SocketChannel>() {
+                        @Override
+                        public void initChannel(SocketChannel ch) {
+                            ChannelPipeline p = ch.pipeline();
+                            p.addLast(new StringDecoder());
+                            p.addLast(new StringEncoder());
+
+                            // Клиентский обработчик.
+                            p.addLast(new UserHandler());
+
+                        }
+                    });
+
+            ChannelFuture f = b.connect(HOST, PORT).sync();
+
+            String input = "Evgeniy";
+            Channel channel = f.sync().channel();
+            channel.writeAndFlush(input);
+            channel.flush();
+            f.channel().closeFuture().sync();
+        } finally {
+            group.shutdownGracefully();
+        }
+    }
+}
